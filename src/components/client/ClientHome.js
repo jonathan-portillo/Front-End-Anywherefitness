c1f7cb282990c211b1d396e0f0f84cafd9304efe
jonathan-portillo <54336208+jonathan-portillo@users.noreply.github.com>
2023-09-28 00:33:39 -0400

    styling

--- src/components/client/ClientHome.js
+++ src/components/client/ClientHome.js
@@ -1,10 +1,10 @@
 import React, { useEffect, useState } from "react";
 import Search from "./Search";
 import { connect } from "react-redux";
-import ClassCard from './ClassCard';
+import ClassCard from "./ClassCard";
 import { fetchClasses } from "../../actions/index";
 // import '../styles/ClientStyle.css';
-import '../../App.css';
+import "../../App.css";
 
 const divStyle = {
   display: "flex",
@@ -12,19 +12,19 @@ const divStyle = {
   flexDirection: "row",
   margin: "0 auto",
   flexWrap: "wrap",
-  justifyContent: "center"
+  justifyContent: "center",
 };
 
-const ClientHome = props => {
+const ClientHome = (props) => {
   const [query, setQuery] = useState("");
   const [displayedClasses, setDisplayedClasses] = useState([]);
   const fetchClasses = props.fetchClasses;
 
-  const handleInputChange = event => {
+  const handleInputChange = (event) => {
     setQuery(event.target.value);
   };
 
-  useEffect(()=> {
+  useEffect(() => {
     fetchClasses();
   }, [fetchClasses]);
 
@@ -32,7 +32,7 @@ const ClientHome = props => {
     if (query.length < 1) {
       setDisplayedClasses(props.classes);
     } else {
-      let searchResults = displayedClasses.filter(c => {
+      let searchResults = displayedClasses.filter((c) => {
         return (
           c.class_name.toLowerCase().includes(query.toLowerCase()) ||
           c.class_type.toLowerCase().includes(query.toLowerCase()) ||
@@ -47,19 +47,19 @@ const ClientHome = props => {
     }
   }, [props.classes, query]);
 
+  const clientName = localStorage.getItem("username");
+
   return (
     <div id="client-page">
       <div className="background">
         <br />
-        <h1>Client Dashboard</h1>
-        <Search handleInputChange={handleInputChange} query={query} />
+        <div className="title">
+          <h1> {clientName}'s Dashboard</h1>
+          <Search handleInputChange={handleInputChange} query={query} />
+        </div>
         <div style={divStyle}>
           {displayedClasses.map((klass, index) => (
-            <ClassCard 
-            key={index} 
-            klass={klass}
-            index={index}
-            />
+            <ClassCard key={index} klass={klass} index={index} />
           ))}
         </div>
       </div>
@@ -67,10 +67,10 @@ const ClientHome = props => {
   );
 };
 
-const mapStateToProps = state => {
+const mapStateToProps = (state) => {
   return {
     classes: state.classes,
-    scheduledClasses: state.scheduledClasses
+    scheduledClasses: state.scheduledClasses,
   };
 };
 export default connect(mapStateToProps, { fetchClasses })(ClientHome);

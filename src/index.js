c75ba28b2ba334747956227768240291a587f0ec
jonathan-portillo <54336208+jonathan-portillo@users.noreply.github.com>
2023-09-29 00:56:44 -0400

    found problem in store

--- src/index.js
+++ src/index.js
@@ -1,29 +1,34 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
+import React from "react";
+import ReactDOM from "react-dom";
 import { BrowserRouter as Router } from "react-router-dom";
-import { applyMiddleware, createStore, compose } from 'redux';
-import { Provider } from 'react-redux';
-import { classReducer } from './reducers/index';
-import thunk from 'redux-thunk';
-import 'bootstrap/dist/css/bootstrap.min.css';
-import './index.css';
-import App from './App';
+import { applyMiddleware, createStore, compose } from "redux";
+import { Provider } from "react-redux";
+import { classReducer } from "./reducers/index";
+import thunk from "redux-thunk";
+import "bootstrap/dist/css/bootstrap.min.css";
+import "./index.css";
+import App from "./App";
 
-import reportWebVitals from './reportWebVitals';
+import reportWebVitals from "./reportWebVitals";
 
-const middleware = [thunk]
-const store = createStore(classReducer,
-  compose(applyMiddleware(...middleware),
-  window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__())
+const middleware = [thunk];
+const store = createStore(
+  classReducer,
+  compose(
+    applyMiddleware(...middleware),
+    window.__REDUX_DEVTOOLS_EXTENSION__
+      ? window.__REDUX_DEVTOOLS_EXTENSION__()
+      : (f) => f
+  )
 );
 
 ReactDOM.render(
   <Provider store={store}>
     <Router>
-    <App />
+      <App />
     </Router>
   </Provider>,
-  document.getElementById('root')
+  document.getElementById("root")
 );
 
 // If you want to start measuring performance in your app, pass a function
